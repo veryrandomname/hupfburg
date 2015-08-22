50041426ca369ad28b136eaf3e353e37fe531fe4
derfailer <chuck.kensy@gmail.com>
2015-08-22 19:46:44 +0200

    added red to exploding

--- bomb.lua
+++ bomb.lua
@@ -12,7 +12,11 @@ function bomb.new(x,y,r)
 end
 
 function bomb.draw(b)
-	love.graphics.setColor(100+b.age*20,100+b.age*20,100+b.age*20)
+  if b.explodes then
+  	love.graphics.setColor(100+b.age*20,100-b.age*20,100-b.age*20)
+  else
+	  love.graphics.setColor(100+b.age*20,100+b.age*20,100+b.age*20)
+  end
   --love.graphics.circle("fill", b.body:getX(), b.body:getY(), b.shape:getRadius())
   
 	love.graphics.draw(bomb1, b.body:getX(), b.body:getY(), b.body:getAngle(), b.shape:getRadius()/bomb1:getWidth()*2, b.shape:getRadius()/bomb1:getWidth()*2, bomb1:getWidth()/2, bomb1:getHeight()/2)
