5b87eff893166dfdca12c1c454118c396e2b6b3e
derfailer <chuck.kensy@gmail.com>
2015-08-22 14:59:29 +0200

    adding goals, with physical bodys

---
+++ burg.lua
@@ -0,0 +1,57 @@
+
+local burg = {}
+burg.size = 100
+burg.wallsize = 15
+players = {}
+
+function burg.load()
+  love.physics.setMeter(64) --the height of a meter our worlds will be 64px
+  world = love.physics.newWorld(0, 9.81*64, true)
+  burgen = {burg.new(love.graphics.getWidth()- burg.size- burg.wallsize ,love.graphics.getHeight() - burg.wallsize- burg.size),
+burg.new(0 ,love.graphics.getHeight()- burg.wallsize - burg.size)}
+
+
+
+end
+function burg.new(x,y)
+  local new = {}
+  new.walls = {}
+  new.walls.b = {}
+  new.walls.b.l = love.physics.newBody(world, x, y) --left wall
+  new.walls.b.r = love.physics.newBody(world, x + burg.size, y) --right wall
+  new.walls.b.g = love.physics.newBody(world, x, y + burg.size) --ground
+
+  new.walls.s = {}
+  new.walls.s.l = love.physics.newRectangleShape(burg.wallsize, burg.size) 
+  new.walls.s.r = love.physics.newRectangleShape(burg.wallsize, burg.size)
+  new.walls.s.g = love.physics.newRectangleShape(burg.size, burg.wallsize)
+
+  new.walls.f = {}
+  new.walls.f.l = love.physics.newFixture(new.walls.b.l, new.walls.s.l) --attach shape to body
+  new.walls.f.r = love.physics.newFixture(new.walls.b.r, new.walls.s.r) --attach shape to body
+  new.walls.f.g = love.physics.newFixture(new.walls.b.g, new.walls.s.g) --attach shape to body
+
+  new.coll = {}
+  new.coll.b = love.physics.newBody(world, x, y)
+  new.coll.s = love.physics.newRectangleShape(burg.size, burg.size)
+  new.coll.f = love.physics.newFixture(new.coll.b, new.coll.s)
+  new.coll.f:setSensor(true)
+  return new
+
+end
+function burg.update(dt)
+  world:update(dt)
+
+
+
+end
+function burg.draw()
+  --print("lol")
+	for i, v in pairs(burgen) do    
+	  love.graphics.rectangle( "fill", v.walls.b.l:getX(), v.walls.b.l:getY(), burg.wallsize, burg.size)
+	  love.graphics.rectangle( "fill", v.walls.b.r:getX(), v.walls.b.r:getY(), burg.wallsize, burg.size)
+	  love.graphics.rectangle( "fill", v.walls.b.g:getX(), v.walls.b.g:getY(), burg.size, burg.wallsize)
+	end
+end
+print("return")
+return burg 
