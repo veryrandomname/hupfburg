90ee13e8b46dfde1d338e49aeb8f2de7de9c0943
veryrandomname <seriousemailadress@googlemail.com>
2015-08-22 20:30:13 +0200

    color bombs and bubbles

--- bomb.lua
+++ bomb.lua
@@ -14,17 +14,17 @@ end
 function bomb.draw(b)
   if b.explodes then
   	love.graphics.setColor(100+b.age*20,100-b.age*20,100-b.age*20)
-
   	love.graphics.draw(bomb1, b.body:getX(), b.body:getY(), b.body:getAngle(), b.shape:getRadius()/bomb1:getWidth()*2, b.shape:getRadius()/bomb1:getWidth()*2, bomb1:getWidth()/2, bomb1:getHeight()/2)
+  	love.graphics.setColor(100+b.age*20,100-b.age*20,100-b.age*20,b.age*30)
+    love.graphics.circle("fill", b.body:getX(), b.body:getY(), b.shape:getRadius()-5)
   else
-	  love.graphics.setColor(150+b.age*20,150+b.age*20,150+b.age*20)
+	  love.graphics.setColor(150+b.age*20,150+b.age*20,150+b.age*20,255-b.age*50)
 	  love.graphics.draw(bubble1, b.body:getX(), b.body:getY(), b.body:getAngle(), b.shape:getRadius()/bubble1:getWidth()*2, b.shape:getRadius()/bubble1:getWidth()*2, bubble1:getWidth()/2, bubble1:getHeight()/2)
+	  --love.graphics.setColor(150+b.age*20,150+b.age*20,150+b.age*20, b.age*20)
+    --love.graphics.circle("fill", b.body:getX(), b.body:getY(), b.shape:getRadius())
   end
   
   --love.graphics.circle("fill", b.body:getX(), b.body:getY(), b.shape:getRadius())
-  
-
-
 end
 
 return bomb
