ad05269b5ade53362817e01eea908dff24f8c3b5
derfailer <chuck.kensy@gmail.com>
2015-08-22 20:17:35 +0200

    white bubbles:

--- bomb.lua
+++ bomb.lua
@@ -17,7 +17,7 @@ function bomb.draw(b)
 
   	love.graphics.draw(bomb1, b.body:getX(), b.body:getY(), b.body:getAngle(), b.shape:getRadius()/bomb1:getWidth()*2, b.shape:getRadius()/bomb1:getWidth()*2, bomb1:getWidth()/2, bomb1:getHeight()/2)
   else
-	  love.graphics.setColor(100+b.age*20,100+b.age*20,100+b.age*20)
+	  love.graphics.setColor(150+b.age*20,150+b.age*20,150+b.age*20)
 	  love.graphics.draw(bubble1, b.body:getX(), b.body:getY(), b.body:getAngle(), b.shape:getRadius()/bubble1:getWidth()*2, b.shape:getRadius()/bubble1:getWidth()*2, bubble1:getWidth()/2, bubble1:getHeight()/2)
   end
   
